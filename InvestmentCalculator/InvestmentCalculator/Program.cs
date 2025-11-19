c4554747ae0ccff710ac115c233c7b4e66a9593f
Vibhanshu Vinay <VINA@simcorp.com>
2025-11-19 14:03:15 +0530

    Done Homework1 and done InvestmentCalculator

---
+++ InvestmentCalculator/InvestmentCalculator/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InvestmentCalculator;
+
+namespace InvestmentCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int nominal;
+            decimal price;
+            TransactionType transactionType;
+            decimal currentValue;
+
+            Console.Write("Input the nominal value");
+            string userInput = Console.ReadLine();
+            nominal = int.Parse(userInput);
+
+            Console.Write("Input the price: ");
+            userInput = Console.ReadLine();
+            price = decimal.Parse(userInput);
+
+            Console.Write("Input the transaction type: ");
+            userInput = Console.ReadLine();
+            transactionType = (TransactionType)Enum.Parse(typeof(TransactionType), userInput, true);
+
+            int sign = transactionType == TransactionType.Buy ? 1 : -1;
+            currentValue = sign * nominal * price;
+
+            Console.WriteLine($"Transaction type: {transactionType}");
+            Console.WriteLine($"Nominal: {nominal}");
+            Console.WriteLine($"Price: {price}");
+            Console.WriteLine($"Current value (CV) = {currentValue}");
+
+            Console.ReadKey();
+        }
+    }
+}
